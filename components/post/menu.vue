c4ef4ecb55e49209cac62aee2cd07205bac84859
侯建勇 <953952333@qq.com>
2019-07-23 15:37:44 +0800

    旅游攻略小修改

---
+++ components/post/menu.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="navs">
+        <div></div>
+        <div class="citys">
+            <span></span>
+            <span></span>
+        </div>
+        <div class="imgs"></div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,15 +1,31 @@
 
 <template>
-    <div>旅游攻略首页</div>
+  <el-col class="main">
+    <el-row type="flex" justify="space-between" class="main-fiex">
+      <el-col :span="7">
+        <!-- 左侧菜单栏 -->
+        
+      </el-col>
+      <el-col :span="17">
+        <!-- 右侧文章显示部分 -->
+      </el-col>
+    </el-row>
+  </el-col>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+     
+    };
+  }
+};
 </script>
 
 <style lang="less" scoped>
-
+.main {
+  width: 1000px;
+  margin: 0 auto;
+}
 </style>
-
